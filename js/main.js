24d781cd4c846bf9994b85a17ab7f2971cae1b1e
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2014-05-19 20:20:49 -0300

    Dual Canvas Example

--- js/main.js
+++ js/main.js
@@ -1,32 +1,60 @@
 define(['canvas-webgl-utility/canvas-webgl-utility'],
 function(CanvasWebGLUtility)
 {
-	var canvasWebGLUtility;
+	var main_screen;
+	var second_screen;
 	
 	var drawAxis = function()
 	{
 		// X Axe
-		canvasWebGLUtility.setDrawingColor([0,1,0,1]);
-		canvasWebGLUtility.drawVertices2d(canvasWebGLUtility.webgl_context.LINES,[-1,0,1,0],2);
+		main_screen.setDrawingColor([0,1,0,1]);
+		main_screen.drawVertices2d(main_screen.webgl_context.LINES,[-1,0,1,0],2);
 		
 		//Y Axe
-		canvasWebGLUtility.setDrawingColor([0,0,1,1]);
-		canvasWebGLUtility.drawVertices2d(canvasWebGLUtility.webgl_context.LINES,[0,-1,0,1],2);
+		main_screen.setDrawingColor([0,0,1,1]);
+		main_screen.drawVertices2d(main_screen.webgl_context.LINES,[0,-1,0,1],2);
+	}
+	
+	var drawAxis2 = function()
+	{
+		// X Axe
+		second_screen.setDrawingColor([0,1,0,1]);
+		second_screen.drawVertices2d(second_screen.webgl_context.LINES,[-1,0,1,0],2);
+		
+		//Y Axe
+		second_screen.setDrawingColor([0,0,1,1]);
+		second_screen.drawVertices2d(second_screen.webgl_context.LINES,[0,-1,0,1],2);
 	}
 	
 	var display = function()
 	{
-		canvasWebGLUtility.webgl_context.clearColor(0.0, 0.0, 0.0, 1.0);
-        	canvasWebGLUtility.webgl_context.clear(canvasWebGLUtility.webgl_context.COLOR_BUFFER_BIT | canvasWebGLUtility.webgl_context.DEPTH_BUFFER_BIT);
+		main_screen.webgl_context.clearColor(0.0, 0.0, 0.0, 1.0);
+        	main_screen.webgl_context.clear(main_screen.webgl_context.COLOR_BUFFER_BIT | main_screen.webgl_context.DEPTH_BUFFER_BIT);
 
-		canvasWebGLUtility.pOrtho(-1,1,-1,1,-1,1);
-		canvasWebGLUtility.mvLoadIdentity();
+		main_screen.pOrtho(-1,1,-1,1,-1,1);
+		main_screen.mvLoadIdentity();
 			
-		canvasWebGLUtility.mvPushMatrix();
+		main_screen.mvPushMatrix();
 
 			drawAxis();
 
-		canvasWebGLUtility.mvPopMatrix();	
+		main_screen.mvPopMatrix();	
+	}
+	
+	var display2 = function()
+	{
+		second_screen.webgl_context.clearColor(0.0, 0.0, 0.0, 1.0);
+        	second_screen.webgl_context.clear(second_screen.webgl_context.COLOR_BUFFER_BIT | second_screen.webgl_context.DEPTH_BUFFER_BIT);
+
+		second_screen.pOrtho(-1,1,-1,1,-1,1);
+		second_screen.mvLoadIdentity();
+			
+		second_screen.mvPushMatrix();
+		second_screen.mvRotate(30,[0,0,1]);
+
+			drawAxis2();
+
+		second_screen.mvPopMatrix();	
 	}
 	
 	var loop = function(timeElapsed)
@@ -38,12 +66,18 @@ function(CanvasWebGLUtility)
 	{
 	
 		var canvas = document.getElementById("main-canvas");
-		canvasWebGLUtility = new CanvasWebGLUtility(canvas);
-
+		main_screen = new CanvasWebGLUtility(canvas);
 		
-		canvasWebGLUtility.setDisplayFunction(display);
-		canvasWebGLUtility.setLoopFunction(loop);
-		canvasWebGLUtility.initializeMainLoop();
+		var canvas2 = document.getElementById("second-canvas");
+		second_screen = new CanvasWebGLUtility(canvas2);
+
+		main_screen.setDisplayFunction(display);
+		main_screen.setLoopFunction(loop);
+		main_screen.initializeMainLoop();
+
+		second_screen.setDisplayFunction(display2);
+		second_screen.setLoopFunction(loop);
+		second_screen.initializeMainLoop();
 	}
 	
 	return { main : main }
