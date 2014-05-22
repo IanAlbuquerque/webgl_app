676ee9190129317f6a8c823481f57077ebf4e784
IanAlbuquerque <ian.albuquerque.silva@gmail.com>
2014-05-21 21:52:51 -0300

    Buffers Documentation

--- js/canvas-webgl-utility/buffers/array-webgl-buffer.js
+++ js/canvas-webgl-utility/buffers/array-webgl-buffer.js
@@ -1,39 +1,68 @@
 define(
-function(){
+function()
+{
 
 	/**
-	* A webgl array buffer. Used to store a variety of data needed for redering scenes.
-	* @param {number[]} data The array containing the data to be inserted into the buffer.
-	* @param {number} itemSize The number of entries in the array that defines an unique element.
-	* @param {number} numItems The number of unique elements in the array.
+	* A WebGL array buffer. Used to store a variety of data needed for setting up scenes.
+	* @param {WebGLContext} webgl_context The WebGL canvas context that will be used for 
+	* loading the matrices.
+	* @param {number[]} _data The array containing the data to be inserted into the buffer.
+	* @param {number} _item_size The number of entries in the array that defines an unique element.
+	* @param {number} _number_of_items The number of unique elements in the array.
 	* @class ArrayWebGLBuffer
 	**/
 	var ArrayWebGLBuffer = function(webgl_context,_data,_item_size,_number_of_items)
+	/** @lends ArrayWebGLBuffer# */
 	{
-		var object = this;
+		var that = this;
+
+		/*
+		-------------------------------------------------------------------------------
+		 PRIVATE:
+		-------------------------------------------------------------------------------
+		*/
+
+		/**
+		* The data of the buffer. It should contain 
+		* [item_size]{@link ArrayWebGLBuffer#item_size} x 
+		* [number_of_items]{@link ArrayWebGLBuffer#number_of_items} entries.
+		* @memberOf ArrayWebGLBuffer#
+		* @type {number[]}
+		* @private
+		**/
+		var data = _data;
+
+		/*
+		-------------------------------------------------------------------------------
+		 PUBLIC:
+		-------------------------------------------------------------------------------
+		*/
 		
 		/**
-		* The buffer itself.
-		**/
-		object.content = webgl_context.createBuffer();
-		
-		/**
-		* The array containing the data to be inserted into the buffer.
-		**/
-		object.data = _data;
+		* The WebGL buffer itself.
+		* @type {glBuffer}
+		* @public
+		*/
+		that.content = webgl_context.createBuffer();
 		
 		/**
 		* The number of entries in the array that defines an unique element.
+		* @type {number}
+		* @public
 		**/
-		object.item_size = _item_size;
+		that.item_size = _item_size;
 		
 		/**
 		* The number of unique elements in the array.
+		* @type {number}
+		* @public
 		**/
-		object.number_of_items = _number_of_items; // checar se esse membro de dados realmente é utilizado
+		// This member might be not being used.
+		that.number_of_items = _number_of_items;
 		
-		webgl_context.bindBuffer(webgl_context.ARRAY_BUFFER, object.content);
-		webgl_context.bufferData(webgl_context.ARRAY_BUFFER, new Float32Array(object.data), webgl_context.STATIC_DRAW);
+		// Sets up the buffer.
+		webgl_context.bindBuffer(webgl_context.ARRAY_BUFFER, that.content);
+		webgl_context.bufferData(webgl_context.ARRAY_BUFFER, new Float32Array(data), webgl_context.STATIC_DRAW);
 	}
 	return ArrayWebGLBuffer;	
 });
--- js/canvas-webgl-utility/buffers/element-array-webgl-buffer.js
+++ js/canvas-webgl-utility/buffers/element-array-webgl-buffer.js
@@ -1,38 +1,68 @@
 define(
-function(){
-
+function()
+{
 	/**
-	* A webgl element array buffer. Used to store a variety of data in a index-like format needed for redering scenes.
-	* @param {number[]} data The array containing the data to be inserted into the buffer.
-	* @param {number} itemSize The number of entries in the array that defines an unique element.
-	* @param {number} numItems The number of unique elements in the array.
-	* @class GLElementArrayBuffer
-	**/	
+	* A WebGL element array buffer. Used to store a variety of index-like data needed 
+	* for setting up scenes.
+	* @param {WebGLContext} webgl_context The WebGL canvas context that will be used for 
+	* loading the matrices.
+	* @param {number[]} _data The array containing the data to be inserted into the buffer.
+	* @param {number} _item_size The number of entries in the array that defines an unique element.
+	* @param {number} _number_of_items The number of unique elements in the array.
+	* @class ElementArrayWebGLBuffer
+	**/
 	var ElementArrayWebGLBuffer = function(webgl_context,_data,_item_size,_number_of_items)
+	/** @lends ElementArrayWebGLBuffer# */
 	{
-		var object = this;
+		var that = this;
+
+		/*
+		-------------------------------------------------------------------------------
+		 PRIVATE:
+		-------------------------------------------------------------------------------
+		*/
+
+		/**
+		* The data of the buffer. It should contain 
+		* [item_size]{@link ElementArrayWebGLBuffer#item_size} x 
+		* [number_of_items]{@link ElementArrayWebGLBuffer#number_of_items} entries.
+		* @memberOf ElementArrayWebGLBuffer#
+		* @type {number[]}
+		* @private
+		**/
+		var data = _data;
+
+		/*
+		-------------------------------------------------------------------------------
+		 PUBLIC:
+		-------------------------------------------------------------------------------
+		*/
 		
 		/**
-		* The buffer itself.
-		**/
-		object.content = gl.createBuffer();
+		* The WebGL buffer itself.
+		* @type {glBuffer}
+		* @public
+		*/
+		that.content = webgl_context.createBuffer();
 		
-		/**
-		* The array containing the data to be inserted into the buffer.
-		**/
-		object.data = _data;
 		/**
 		* The number of entries in the array that defines an unique element.
+		* @type {number}
+		* @public
 		**/
-		object.item_size = _item_size;
+		that.item_size = _item_size;
 		
 		/**
 		* The number of unique elements in the array.
+		* @type {number}
+		* @public
 		**/
-		object.number_of_items = _number_of_items; // checar se esse membro de dados realmente é utilizado
+		// This member might be not being used.
+		that.number_of_items = _number_of_items;
 		
-		webgl_context.bindBuffer(webgl_context.ELEMENT_ARRAY_BUFFER, object.content);
-		webgl_context.bufferData(webgl_context.ELEMENT_ARRAY_BUFFER, new Uint16Array(object.data), webgl_context.STATIC_DRAW);
+		// Sets up the buffer.
+		webgl_context.bindBuffer(webgl_context.ELEMENT_ARRAY_BUFFER, that.content);
+		webgl_context.bufferData(webgl_context.ELEMENT_ARRAY_BUFFER, new Uint16Array(data), webgl_context.STATIC_DRAW);
 	}
 
 	return ElementArrayWebGLBuffer;
